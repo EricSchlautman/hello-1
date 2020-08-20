b5f371f0b8852992ca021ed8217915727e3af60e
boverbeek <Boverbeek2@huskers.unl.edu>
2020-08-20 15:34:50 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Brennon Overbeek
+ * Date:    2020/18/08
+ *
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Brennon Overbeek!\n");
+     printf("Computer Science\n");
+
+     return 0;
+
+ }
